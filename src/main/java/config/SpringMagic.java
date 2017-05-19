7d3f0467e9a320f47990e553084b2fcbc27ddba4
Elangovan, Anand <anand.elangovan@sky.uk>
2017-05-19 12:17:21 +0100

    Adding component scan dir

--- src/main/java/config/SpringMagic.java
+++ src/main/java/config/SpringMagic.java
@@ -1,13 +1,11 @@
 package config;
 
-import controller.EmployeeServiceImpl;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controller"})
+@ComponentScan(basePackages = {"com"})
 public class SpringMagic
 {
     public static void main(String[] args) throws Exception {
